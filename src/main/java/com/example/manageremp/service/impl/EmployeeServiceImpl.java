f52c947d1fe8917d899c891ca8ecef2c596152e9
tranductri138 <59952483+tranductri138@users.noreply.github.com>
2021-05-18 17:15:08 +0700

    five commit

--- src/main/java/com/example/manageremp/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/example/manageremp/service/impl/EmployeeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.manageremp.service.impl;
 
 import com.example.manageremp.entity.Employee;
+import com.example.manageremp.exception.NotFoundException;
 import com.example.manageremp.model.EmployeeDto;
 import com.example.manageremp.repository.EmployeeRepo;
 import com.example.manageremp.service.EmployeeService;
@@ -47,8 +48,12 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public EmployeeDto getEmp(Long id) {
         Employee employee = employeeRepo.getEmp(id);
-        EmployeeDto employeeDto = mapper.map(employee, EmployeeDto.class);
-        return employeeDto;
+        if (employee==null){
+            throw new NotFoundException("User không tồn tại trong hệ thống");
+        }else {
+            EmployeeDto employeeDto = mapper.map(employee, EmployeeDto.class);
+            return employeeDto;
+        }
     }
 
 
@@ -57,7 +62,7 @@ public class EmployeeServiceImpl implements EmployeeService {
         List<Employee> employeeList = employeeRepo.searchEmp(name, offset, maxPerPage);
         List<EmployeeDto> employeeDtos = new ArrayList<>();
         if (employeeList.isEmpty()) {
-            return null;
+            throw new NotFoundException("User không tồn tại trong hệ thống");
         } else {
             for (Employee e : employeeList) {
                 employeeDtos.add(mapper.map(e, EmployeeDto.class));
@@ -71,7 +76,7 @@ public class EmployeeServiceImpl implements EmployeeService {
         List<Employee> employeeList = employeeRepo.searchEmp(name);
         List<EmployeeDto> employeeDtoList = new ArrayList<>();
         if (employeeList.isEmpty()){
-            return null;
+            throw new NotFoundException("User không tồn tại trong hệ thống");
         }else {
             for (Employee e: employeeList){
                 employeeDtoList.add(mapper.map(e,EmployeeDto.class));
